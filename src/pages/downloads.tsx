34d48f7d341a2787c9bf72641eb8571e3f62e682
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-08-04 16:37:05 +0200

    feat(header): add links to other pages in header (#10)

---
+++ src/pages/downloads.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import SEO from '../components/seo'
+import Layout from '../layout'
+
+const Downloads: FC = () => {
+  return (
+    <Layout>
+      <SEO title="FAQ" />
+      <span>Téléchargements</span>
+    </Layout>
+  )
+}
+
+export default Downloads
