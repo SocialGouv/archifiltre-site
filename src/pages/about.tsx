ff6c7994b3c998c74baf1f80b8ccd73450c199fe
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-08-06 17:32:57 +0200

    feat(about): add timeline on about page (#16)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,29 @@
 import React, { FC } from 'react'
 import SEO from '../components/seo'
 import Layout from '../layout'
+import Timeline from '@material-ui/lab/Timeline'
+import TimelineElement from '../components/timeline-element'
+import { timelineElements } from '../components/timeline-data'
 
 const Downloads: FC = () => {
   return (
     <Layout>
       <SEO title="FAQ" />
-      <span>A propos</span>
+      <Timeline align="alternate">
+        {timelineElements.map(
+          ({ title, date, link, icon, isMajor = false }, index) => (
+            <TimelineElement
+              title={title}
+              date={date}
+              link={link}
+              icon={icon}
+              isMajor={isMajor}
+              isConnectorDisplayed={index !== timelineElements.length - 1}
+              key={`${date}-${index}`}
+            />
+          ),
+        )}
+      </Timeline>
     </Layout>
   )
 }
