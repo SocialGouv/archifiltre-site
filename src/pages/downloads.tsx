bfa831414edd5ee6c4354ef1e64d3c8f4a7cb231
Alezco <benjaminmorali4@hotmail.fr>
2020-08-20 16:47:43 +0200

    feat: add changelog page

--- src/pages/downloads.tsx
+++ src/pages/downloads.tsx
@@ -4,13 +4,15 @@ import Layout from '../layout'
 import Box from '@material-ui/core/Box'
 import versions from '../display-data/versions'
 import DownloadLink from '../components/download-link'
+import { Link as GatsbyLink } from 'gatsby'
+import { Button } from '@material-ui/core'
 
 const Downloads: FC = () => {
   return (
     <Layout>
       <SEO title="Téléchargements" />
       <Box p={1}>
-        <h2>Téléchargements</h2>
+        <h1>Téléchargements</h1>
         <h3>Script</h3>
         <Box>
           <DownloadLink
@@ -27,7 +29,15 @@ const Downloads: FC = () => {
         {versions.map(version => (
           <div key={`${version.name}-${version.number}`}>
             <h3>
-              {version.name} {version.number} - {version.date}
+              {`${version.name} ${version.number} - ${version.date} - `}
+              <Button
+                color="inherit"
+                component={GatsbyLink}
+                to="/changelog"
+                size="small"
+              >
+                Nouveautés de cette version
+              </Button>
             </h3>
             <Box>
               {version.platforms.map(platform => (
