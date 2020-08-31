1533fb8e4b7242bcd40e5166861b773e6728fb85
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-08-31 11:55:47 +0200

    feat(tracking): add matomo trackers (#50)

--- src/pages/telechargements.tsx
+++ src/pages/telechargements.tsx
@@ -9,53 +9,57 @@ import versions from "../display-data/versions";
 import Layout from "../layout";
 import { getPlatformName, Platform } from "../utils/os-util";
 
-const Telechargements: FC = () => {
-  return (
-    <Layout>
-      <SEO title="Téléchargements" />
-      <Box p={1}>
-        <h1>Téléchargements</h1>
-        <h3>Script</h3>
-        <Box>
-          <DownloadLink
-            url="https://raw.githubusercontent.com/SocialGouv/archifiltre/master/scripts/load-from-filesystem.ps1"
-            label="Windows"
-          />
-        </Box>
-        <Box>
-          <DownloadLink
-            url="https://raw.githubusercontent.com/SocialGouv/archifiltre/master/scripts/load-filesystem.sh"
-            label="Linux/MacOS"
-          />
-        </Box>
-        {versions.map((version) => (
-          <div key={`${version.name}-${version.number}`}>
-            <h3>
-              {`${version.name} ${version.number} - ${version.date} - `}
-              <Button
-                color="inherit"
-                component={GatsbyLink}
-                to="/changelog"
-                size="small"
-              >
-                Nouveautés de cette version
-              </Button>
-            </h3>
-            <Box>
-              {version.platforms.map((platform: Platform) => {
-                const platformName = getPlatformName(platform);
-                return (
-                  <Box key={`${platformName}-${version.number}`}>
-                    <DownloadLink url={platform.url} label={platformName} />
-                  </Box>
-                );
-              })}
-            </Box>
-          </div>
-        ))}
+const Telechargements: FC = () => (
+  <Layout>
+    <SEO title="Téléchargements" />
+    <Box p={1}>
+      <h1>Téléchargements</h1>
+      <h3>Script d&rsquo;import (serveur)</h3>
+      <Box>
+        <DownloadLink
+          url="https://raw.githubusercontent.com/SocialGouv/archifiltre/master/scripts/load-from-filesystem.ps1"
+          label="Windows"
+          version="Script"
+        />
       </Box>
-    </Layout>
-  );
-};
+      <Box>
+        <DownloadLink
+          url="https://raw.githubusercontent.com/SocialGouv/archifiltre/master/scripts/load-filesystem.sh"
+          label="Linux/MacOS"
+          version="Script"
+        />
+      </Box>
+      {versions.map((version) => (
+        <div key={`${version.name}-${version.number}`}>
+          <h3>
+            {`${version.name} ${version.number} - ${version.date} - `}
+            <Button
+              color="inherit"
+              component={GatsbyLink}
+              to="/changelog"
+              size="small"
+            >
+              Nouveautés de cette version
+            </Button>
+          </h3>
+          <Box>
+            {version.platforms.map((platform: Platform) => {
+              const platformName = getPlatformName(platform);
+              return (
+                <Box key={`${platformName}-${version.number}`}>
+                  <DownloadLink
+                    url={platform.url}
+                    label={platformName}
+                    version={version.number}
+                  />
+                </Box>
+              );
+            })}
+          </Box>
+        </div>
+      ))}
+    </Box>
+  </Layout>
+);
 
 export default Telechargements;
