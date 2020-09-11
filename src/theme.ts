f6fd9c7ed1635d48c1dcf8b98e3d5a50dc8507d7
Alezco <benjaminmorali4@hotmail.fr>
2020-09-11 16:31:17 +0200

    feat: update website font

--- src/theme.ts
+++ src/theme.ts
@@ -7,6 +7,22 @@ import {
 } from "@material-ui/core/styles";
 import deepMerge from "deepmerge";
 
+import OpenSans from "../static/fonts/OpenSans-Regular.ttf";
+
+const openSans = {
+  fontDisplay: "swap",
+  fontFamily: "OpenSans",
+  fontStyle: "normal",
+  fontWeight: 400,
+  src: `
+    local('OpenSans'),
+    local('OpenSans-Regular'),
+    url(${OpenSans}) format('ttf')
+  `,
+  unicodeRange:
+    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+};
+
 const darkBlue = {
   contrastText: "#fff",
   dark: "#202a54",
@@ -17,9 +33,19 @@ const darkBlue = {
 const makeTheme = (variant: ThemeOptions): Theme => {
   const type = variant?.palette?.type;
   const common = {
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          "@font-face": [openSans],
+        },
+      },
+    },
     palette: {
       primary: type === "light" ? darkBlue : blue,
       secondary: grey,
+    },
+    typography: {
+      fontFamily: "OpenSans, Arial",
     },
   };
   const theme = createMuiTheme(deepMerge(common, variant));
