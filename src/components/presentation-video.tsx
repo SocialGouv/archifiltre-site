d29a0e978ca16feaaec2aa08dd89e35e8a9e6a6b
Alezco <benjaminmorali4@hotmail.fr>
2020-12-14 11:10:20 +0100

    feat(product): add video on product page

---
+++ src/components/presentation-video.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PresentationVideo = () => (
+  <iframe
+    width="840"
+    height="472"
+    src="https://www.youtube.com/embed/dhGG_-JPo2E"
+    frameBorder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen
+  />
+);
+export default PresentationVideo;
