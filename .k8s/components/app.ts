6ac93f5cbe0a1cab7702a1dcc8c1b5d8c6178608
Clément Berthou <cberthou@users.noreply.github.com>
2021-03-03 12:38:19 +0100

    chore(k8s): update ingress caching policy (#146)

--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -1,9 +1,21 @@
 import env from "@kosko/env";
 
 import { create } from "@socialgouv/kosko-charts/components/nginx";
+import type { Ingress } from "kubernetes-models/api/networking/v1beta1/Ingress";
 
 const manifests = create("app", {
   env,
 });
+
+// @ts-ignore
+const ingress = manifests.find(({ kind }) => kind === "Ingress") as Ingress;
+
+ingress.metadata = {
+  ...ingress.metadata,
+  annotations: {
+    ...ingress.metadata?.annotations,
+    "nginx.ingress.kubernetes.io/configuration-snippet": `if ($request_uri ~* \\.(js|css|gif|jpe?g|png)) { expires 1y; add_header Cache-Control "max-age=31556952, public"; }`
+  },
+}
 
 export default manifests;
