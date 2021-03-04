d9116b28901154db06661f476e696dba961169b5
Clément Berthou <cberthou@users.noreply.github.com>
2021-03-04 16:45:50 +0100

    feat(stats): add piecharts support (#152)

---
+++ src/components/statistic-piecharts.tsx
@@ -0,0 +1,67 @@
+import { EventTracker, Palette } from "@devexpress/dx-react-chart";
+import {
+  Chart,
+  Legend,
+  PieSeries,
+  Tooltip,
+} from "@devexpress/dx-react-chart-material-ui";
+import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import React, { FC, useCallback, useState } from "react";
+
+import { PiechartStatisticConfig } from "../types/statistic-types";
+
+type StatisticPiechartProps = {
+  statistic: PiechartStatisticConfig;
+};
+
+// Default MUI palette for dx-react-chart
+const palette = [
+  "#42A5F5",
+  "#FF7043",
+  "#9CCC65",
+  "#FFCA28",
+  "#26A69A",
+  "#EC407A",
+];
+
+const StatisticPiechart: FC<StatisticPiechartProps> = ({ statistic }) => {
+  const [targetItem, setTargetItem] = useState();
+  const chartData = statistic.value.map(({ label, value }, index) => ({
+    key: index,
+    label,
+    value,
+  }));
+
+  const onTargetItemChange = useCallback(
+    (newTargetItem) => setTargetItem(newTargetItem),
+    [setTargetItem]
+  );
+
+  return (
+    <Chart data={chartData} height={140}>
+      <Palette scheme={palette} />
+      <PieSeries valueField="value" argumentField="label" />
+      <EventTracker />
+      <Tooltip
+        targetItem={targetItem}
+        onTargetItemChange={onTargetItemChange}
+        contentComponent={() => (
+          <List>
+            {chartData.map(({ label, value }, index) => (
+              <ListItem key={index}>
+                <ListItemIcon>
+                  <Legend.Marker color={palette[index]} />
+                </ListItemIcon>
+                <ListItemText>
+                  {label} : {value}
+                </ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        )}
+      />
+    </Chart>
+  );
+};
+
+export default StatisticPiechart;
--- src/components/statistic.tsx
+++ src/components/statistic.tsx
@@ -1,13 +1,26 @@
 import React, { FC } from "react";
 
-import { isRawStatistic, StatisticConfig } from "../types/statistic-types";
+import {
+  isPiechart,
+  isRawStatistic,
+  StatisticConfig,
+} from "../types/statistic-types";
+import StatisticPiechart from "./statistic-piecharts";
 import StatisticRaw from "./statistic-raw";
 
 type StatisticProps = {
   statistic: StatisticConfig;
 };
 
-const Statistic: FC<StatisticProps> = ({ statistic }) =>
-  isRawStatistic(statistic) ? <StatisticRaw statistic={statistic} /> : null;
+const Statistic: FC<StatisticProps> = ({ statistic }) => {
+  if (isPiechart(statistic)) {
+    return <StatisticPiechart statistic={statistic} />;
+  }
+  if (isRawStatistic(statistic)) {
+    return <StatisticRaw statistic={statistic} />;
+  }
+
+  return null;
+};
 
 export default Statistic;
