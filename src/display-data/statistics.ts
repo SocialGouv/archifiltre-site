8e6e5e02e56ad75156a088ea8730c5ae42dc0a64
Clément Berthou <cberthou@users.noreply.github.com>
2021-03-23 16:11:33 +0100

    chore(stats): remove unused config (#163)

--- src/display-data/statistics.ts
+++ src/display-data/statistics.ts
@@ -1,8 +1,4 @@
-import {
-  Formatting,
-  StatisticConfig,
-  StatisticsGroup,
-} from "../types/statistic-types";
+import { Formatting, StatisticsGroup } from "../types/statistic-types";
 
 export const statisticsLayout: StatisticsGroup[] = [
   {
@@ -325,53 +321,5 @@ export const statisticsLayout: StatisticsGroup[] = [
       },
     ],
     title: "Vues sur les autres canaux 👀",
-  },
-];
-
-export const statisticsConfig: StatisticConfig[] = [
-  {
-    fields: ["download", "appDownload"],
-    label: "Téléchargements d'Archifiltre",
-    type: "aggregated",
-  },
-  {
-    fields: ["CSV Export", "CSV with hashes Export", "Tree CSV Export"],
-    label: "Exports CSV (avec empreinte...)",
-    type: "aggregated",
-  },
-  {
-    field: "Excel Export",
-    label: "Exports Excel (depuis le 26/10/2020)",
-    type: "simple",
-  },
-  {
-    fields: ["METS Export", "RESIP Export"],
-    label: "Exports METS/RESIP (pour transfert vers SAE)",
-    type: "aggregated",
-  },
-  {
-    field: "FileTreeDrop",
-    label: "Analyses effectuées",
-    type: "simple",
-  },
-  {
-    field: "Audit report export",
-    label: "Rapports d'audit générés",
-    type: "simple",
-  },
-  {
-    field: "youtubeViews",
-    label: "Vues sur la chaîne Youtube",
-    type: "simple",
-  },
-  {
-    field: "visitsCount",
-    label: "Visites sur le site",
-    type: "simple",
-  },
-  {
-    field: "wikiViews",
-    label: "Vues sur le Wiki Archifiltre (14 derniers jours)",
-    type: "simple",
   },
 ];
