06e2887c14ad341fa32230d5db14520f5fc72228
mehdilouraoui <med.louraoui@gmail.com>
2023-11-14 16:44:27 +0100

    feat/redirect-product

--- src/pages/produit.tsx
+++ src/pages/produit.tsx
@@ -3,13 +3,11 @@ import React from "react";
 import SEO from "../components/seo";
 import { useRedirect } from "../hooks/useRedirect";
 
-React;
-
 const Produit = () => {
   useRedirect("/");
   return (
     <>
-      <SEO title="Produit" />;
+      <SEO title="Produit" />
     </>
   );
 };
